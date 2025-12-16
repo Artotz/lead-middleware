626ee00a2ec5ce8076923b946c59c1ee21c03391
Artur Catunda <arturmcatunda@gmail.com>
2025-12-16 15:55:32 -0300

    stopped using service role key (duh)

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -9,11 +9,11 @@ export function getSupabaseServerClient(): SupabaseClient {
 
   const url = process.env.SUPABASE_URL;
   const serviceRoleKey =
-    process.env.SUPABASE_SERVICE_ROLE_KEY ?? process.env.SUPABASE_ANON_KEY;
+    process.env.SUPABASE_ANON_KEY ?? process.env.SUPABASE_SERVICE_ROLE_KEY;
 
   if (!url || !serviceRoleKey) {
     throw new Error(
-      "Supabase URL e chave não encontradas. Defina SUPABASE_URL e SUPABASE_SERVICE_ROLE_KEY (ou SUPABASE_ANON_KEY).",
+      "Supabase URL e chave não encontradas. Defina SUPABASE_URL e SUPABASE_SERVICE_ROLE_KEY (ou SUPABASE_ANON_KEY)."
     );
   }
 
